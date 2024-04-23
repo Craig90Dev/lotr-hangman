1e9343be5418e243a265d8936f0fd9498254a508
Craig Farran <fazzer_70@hotmail.co.uk>
2024-04-23 11:44:43 +0000

    Feat: Add validation to check for more than one character length in validate_data_instructions function

--- run.py
+++ run.py
@@ -19,8 +19,24 @@ def start_game():
     print("Welcome to LOTR Hangman!")
     print("Do you want to view the instructions?\n")
 
-    decision = input("Please enter either Y or N:\n")
-    print(f"You have selected {decision}")
+    view_instructions = input("Please enter either Y or N:\n")
+    validate_data_instructions(view_instructions)
+
+    # print(f"You have selected {view_instructions}")
     
+def validate_data_instructions(y_or_n):
+    """
+    Inside the try, ensures the players choice is either a Y or N.
+    Raises a ValueError if the input is invalid or if there is more
+    than one value.
+    """
+    try:
+        if len(y_or_n) != 1:
+            raise ValueError(
+                f"Only 1 letter accepted, you provided {len(y_or_n)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please enter Y or N.\n")
+
 
 start_game()
