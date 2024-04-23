d3d6359d57e6ce76590789a347ebe5c25478f9bf
Craig Farran <fazzer_70@hotmail.co.uk>
2024-04-23 13:42:26 +0000

    Feat: Add loopback for invalid data

--- run.py
+++ run.py
@@ -18,9 +18,13 @@ def start_game():
     """
     print("Welcome to LOTR Hangman!")
     print("Do you want to view the instructions?\n")
+    while True:
+        
+        view_instructions = input("Please enter either Y or N:\n")
 
-    view_instructions = input("Please enter either Y or N:\n")
-    validate_data_instructions(view_instructions)
+        if validate_data_instructions(view_instructions):
+            print("data valid")
+            break
     
 def validate_data_instructions(y_or_n):
     """
@@ -34,14 +38,27 @@ def validate_data_instructions(y_or_n):
                 f"Only 1 letter accepted, you entered {len(y_or_n)}"
             )
     except ValueError as e:
-        print(f"Invalid answer: {e}.\nPlease enter Y or N.\n")
+        print(f"Invalid answer: {e}.\n")
         return False
     
     if y_or_n.upper() == "Y":
-        print("OKAY")
+        print("Guess each letter of the hidden word.")
+        print("The word is the name of a character from J.R.R Tolkien's Lord Of The Rings series.")
+        print("If you guess correctly the letters will be shown in their appropriate place.")
+        print("If you guess incorrectly a body part will be added to the Hangman image.")
+        print("If you guess the whole word you win!")
+        print("If all body parts on the Hangman diagram are shown you lose.")
+        print("You can try and guess the word at any time during the game.\n")
+        print("Good Luck!\n")
+        grab_word()
     elif y_or_n.upper() == "N":
-        print("SAJ")
+        grab_word()
     else:
-        print(f"You entered {y_or_n.upper()}, please enter Y or N")
+        print(f"You entered {y_or_n.upper()}\n")
+        return False
+    return True
 
+def grab_word():
+    print("WORD")
+    
 start_game()
