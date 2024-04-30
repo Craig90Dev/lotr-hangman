a1bbfab45c250a76ae48767004c185f39b5e6a0a
Craig Farran <fazzer_70@hotmail.co.uk>
2024-04-30 11:58:24 +0000

    Feat: Add play_again function and more comments for further improvement in understanding functions

--- run.py
+++ run.py
@@ -13,26 +13,29 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('lotr_hangman')
 
 
-
 def start_game():
     """
     Asks player if they want to view instructions
     """
     print("Welcome to LOTR Hangman!")
     print("Do you want to view the instructions?\n")
-
     #Loops through validation function and repeats user input until data received returns True
     while True:
         view_instructions = input("Please enter either Y or N:\n")
-        #Breaks from while loop 
         if validate_data_instructions(view_instructions):
-            break
+            #For play_again function, if true then main_game function is called, if false then
+            #game ends.
+            while True:
+                if main_game():
+                    continue
+                else:
+                    print("Thank you for playing!")
+                    return
 
-    main_game()
 
 def validate_data_instructions(y_or_n):
     """
-    Inside the try, ensures the players choice is either a Y or N.
+    Inside the try, ensures the player's choice is either Y or N.
     Raises a ValueError if the input is invalid or if there is more
     than one value.
     """
@@ -46,7 +49,6 @@ def validate_data_instructions(y_or_n):
     except ValueError as e:
         print(f"Invalid answer: {e}.\n")
         return False
-    
     #Returns True if player answers Y or N. Shows instructions if player selects Y, 
     #starts grab_word function if selects N. Returns False and loops back to while
     #statement if player answers anything other than Y or N.
@@ -61,11 +63,10 @@ def validate_data_instructions(y_or_n):
         print("Good Luck!\n")
         grab_word()
     elif y_or_n.upper() == "N":
-        grab_word()
+        return True 
     else:
         print(f"You entered {y_or_n.upper()}\n")
         return False
-    return True
 
 def hide_letters(word):
     """
@@ -81,7 +82,7 @@ def hide_letters(word):
 
 def grab_word():
     """
-    Grabs random word from Google worksheet
+    Grabs random word from Google worksheet, converts to string and returns as chosen word
     """
     words = SHEET.worksheet("chars").get_all_values()
 
@@ -94,71 +95,88 @@ def grab_word():
     
 def main_game():
     """
-    Runs a while loop to iterate through each of the players guesses, updating guessed_letters
+    Runs a while loop to iterate through each of the player's guesses, updating guessed_letters
     variable and updating the hidden word as necessary until the player either guesses the
-    correct word or hits the limit of max_wrong_attempts. While loop breaks when either the player
-    has guessed the word or if all attempts are used up. (Will be updated to play again option)
+    correct word or hits the limit of max_wrong_attempts. While loop calls play_again function when either the player
+    has guessed the word or if all attempts are used up.
     """
-    word = grab_word()
-    hidden_word = hide_letters(word)
-    max_wrong_attempts = 6
-    wrong_attempts = 0
-    guessed_letters = []
-
     while True:
+        word = grab_word()
+        hidden_word = hide_letters(word)
+        max_wrong_attempts = 6
+        wrong_attempts = 0
+        guessed_letters = []
 
-        guessed_letters_shown = " ".join(guessed_letters)
-        print_hangman(wrong_attempts)
-        print(f"Guessed letters: {guessed_letters_shown}")
-        print(hidden_word)
+        while True:
+            guessed_letters_shown = " ".join(guessed_letters)
+            print_hangman(wrong_attempts)
+            print(f"Guessed letters: {guessed_letters_shown}")
+            print(hidden_word)
 
-        guess = input("Guess a letter or the entire word: ").upper()
-        #Player wins if guess matches entire word
-        if guess == word:
-            print("Congratulations! You've guessed the word correctly:", word)
-            break
-        #Checks if the players guessed letter is already in the guessed letters variable
-        elif guess in guessed_letters:
-            print("You've already guessed that letter.")
-            continue
-        #Checks to see if the players guess is a single letter and appends it to the guessed_letters variable
-        elif len(guess) == 1 and guess.isalpha():
-            guessed_letters.append(guess)
-            #if guessed character is in the hidden word, updates the hidden word
-            if guess in word:
-                hidden_word = update_hidden_word(word, hidden_word, guess)
-                #checks to see if the updated hidden word is equal to the word
-                if hidden_word.replace(" ", "") == word:
-                    print("Congratulations! You've guessed the word correctly:", word)
-                    break
-            #If none of the above conditions are met adds +1 to the wrong answers variable
+            guess = input("Guess a letter or the entire word: ").upper()
+            # Player wins if guess matches entire word
+            if guess == word:
+                print("Congratulations! You've guessed the word correctly:", word)
+                return play_again()
+            # Checks if the players guessed letter is already in the guessed letters variable
+            elif guess in guessed_letters:
+                print("You've already guessed that letter.")
+                continue
+            # Checks to see if the players guess is a single letter and appends it to the guessed_letters variable
+            elif len(guess) == 1 and guess.isalpha():
+                guessed_letters.append(guess)
+                # if guessed character is in the hidden word, updates the hidden word
+                if guess in word:
+                    hidden_word = update_hidden_word(word, hidden_word, guess)
+                    # checks to see if the updated hidden word is equal to the word
+                    if hidden_word.replace(" ", "") == word:
+                        print("Congratulations! You've guessed the word correctly:", word)
+                        return play_again()
+                # If none of the above conditions are met adds +1 to the wrong answers variable
+                else:
+                    wrong_attempts += 1
+                    # If the wrong attempt is the last allowed attempt then prints final hangman diagram
+                    # and gives the final answer to the player
+                    if wrong_attempts >= max_wrong_attempts:
+                        print("\n   ------|")
+                        print("     |   O")
+                        print("     |  /|\\")
+                        print("     |  / \\")
+                        print("   ===== ")
+                        print("You've used up all your attempts. The word was:", word)
+                        return play_again()
+            # If the player guesses something that is not a single letter prints an "invalid" statement
             else:
-                wrong_attempts += 1
-                #If the wrong attempt is the last allowed attempt then prints final hangman diagram
-                #and gives the final answer to the player
-                if wrong_attempts >= max_wrong_attempts:
-                    print("\n   ------|")
-                    print("     |   O")
-                    print("     |  /|\\")
-                    print("     |  / \\")
-                    print("   ===== ")
-                    print("You've used up all your attempts. The word was:", word)
-                    break
-        #If the player guesses something that is not a single letter prints an "invalid" statement          
-        else:
-            print("Invalid input. Please enter a single letter or the entire word.")
+                print("Invalid input. Please enter a single letter or the entire word.")
 
 def update_hidden_word(word, hidden_word, guess):
     """
-    Updates the hidden word based on the players guessed letter
+    Updates the hidden word based on the players guessed letter and adds a space to separate
     """
     updated_hidden_word = ""
     for i in range(len(word)):
+        #if guessed letter matches a character in the hidden word, reveals the character
         if word[i] == guess:
             updated_hidden_word += guess + " "
+        #If guessed letter does not match, keeps hidden letter in position
         else:
             updated_hidden_word += hidden_word[2*i] + " "
     return updated_hidden_word
+
+def play_again():
+    """
+    Asks the player if they want to play again and returns True if they do, False otherwise.
+    """
+    while True:
+        print("Do you want to play again?")
+        play_again_input = input("Please enter either Y or N: ").upper()
+        if play_again_input == 'Y':
+            return True
+        elif play_again_input == 'N':
+            return False
+        else:
+            print("Invalid input. Please enter either Y or N.")
+
 
 def print_hangman(wrong):
     """
